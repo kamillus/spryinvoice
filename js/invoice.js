4f80e482bc7793cc7292aa13ac68040f29271856
kamil <web@kamil.ca>
2016-02-15 23:12:27 -0500

    Logo + bug fixes

--- js/invoice.js
+++ js/invoice.js
@@ -14,6 +14,29 @@ app.directive('contenteditable', function() {
   };
 });
 
+app.directive('preventDefault', function() {
+    return function(scope, element, attrs) {
+        $(element).click(function(event) {
+            event.preventDefault();
+        });
+    }
+})
+
+app.directive('bsHolder', function() {
+  return {
+    compile: function (scope, element, attrs) {
+      Holder.run({images: scope[0]})
+    }
+  };
+})
+
+app.directive('scrollEnd', function() {
+  return {
+    compile: function (scope, element, attrs) {
+    }
+  };
+})
+
 app.config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ShowController, templateUrl:'partials/show.html'}).
@@ -27,7 +50,7 @@ app.factory('SharedBroadcast', function($rootScope) {
 
     sharedService.broadcastItem = function(msg,val) {
         this.message = msg;
-		$rootScope.$broadcast(msg, val);
+    $rootScope.$broadcast(msg, val);
     };
 
     return sharedService;
